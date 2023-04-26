ed99dbc596cd860d9cedd80398cedb4bd0fb43a1
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-04-26 09:06:13 -0300

    Renomeando variáveis e limpando o codigo

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const output = document.querySelector("#outputCurrent");
-const previusOperation = document.querySelector("#outputHold");
+const previousOperation = document.querySelector("#outputHold");
 const numsBtn = document.querySelectorAll(".nums");
 const clearBtn = document.querySelector("#clear");
-const clearAllBtn = document.querySelector("#reseat");
+const reseatBtn = document.querySelector("#reseat");
 const operatorBtns = document.querySelectorAll(".operator");
 const result = document.querySelector("#result");
 const dotBtn = document.querySelector("#dotBtn");
@@ -12,7 +12,7 @@ const operations = {
     multiply : (a, b) => decimalAjust(a * b),
     divide :  (a, b) =>  decimalAjust(a / b),
     porcentage : (a, b) =>  decimalAjust((a / 100) * b)
-}
+};
 
 let holdNumber = null;
 let currentNumber = "";
@@ -23,23 +23,22 @@ const keybordInput = (e) => {
     if (e.key == "Backspace") {clear()};
     if (e.key == "Enter" || e.key == "=") {finishOperation()};
     if (e.key == ".") {addDot()};
-    if (e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/" || e.key == "%") {
+    if (e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/" ||
+     e.key == "%") {
         selectOperator(e.key)
-    }
-}
+    };
+};
 
 const addNumber = (num) => {
     currentNumber += num;
     updateOutput(currentNumber);
-}
+};
 
 const addDot = () => {
-    if (currentNumber.includes(".") || !currentNumber) {
-        return
-    }
+    if (currentNumber.includes(".") || !currentNumber) {return};
     currentNumber += ".";
-    updateOutput(currentNumber)
-}
+    updateOutput(currentNumber);
+};
 
 const convertOperator = (operator) => {
     switch (operator) {
@@ -53,15 +52,15 @@ const convertOperator = (operator) => {
             return "divide"
         case "%":
             return "porcentage"
-    }
-}
+    };
+};
 
 function execute (a, b, operator) {
     if (!b) {return}
-    b = Number(b)
+    b = Number(b);
     holdNumber = operations[convertOperator(operator)](a, b);
-    updateOutput(holdNumber)
-}
+    updateOutput(holdNumber);
+};
 
 function selectOperator (operator) {
     currentOperator = operator;
@@ -73,8 +72,8 @@ function selectOperator (operator) {
         execute(holdNumber, currentNumber, currentOperator)
     }
     currentNumber = "";
-    updatePrevius()
-}
+    updatePrevius();
+};
 
 const updateOutput = (number) => {
     if (number == "") {
@@ -82,28 +81,27 @@ const updateOutput = (number) => {
     } else {
         output.innerText = number;
     }
-}
+};
 
 const updatePrevius = () =>  {
     if (holdNumber != null) {
-        previusOperation.innerHTML = holdNumber + " " +  currentOperator ;
+        previousOperation.innerHTML = holdNumber + " " +  currentOperator ;
     } else {
-        previusOperation.textContent = "";
+        previousOperation.textContent = "";
     }
-}
+};
 
 const decimalAjust = (num) =>  {
     return Math.round(num * 1000) / 1000;
-}
+};
 
 const clear = () => {
     currentNumber = output.innerText;
     currentNumber = currentNumber.split("")
     .filter((num, index) => index == currentNumber.length -1 ? false: true)
     .join("");
-    output.innerText = "";
     updateOutput(currentNumber);
-}
+};
 
 const reseat = () => {
     holdNumber = null;
@@ -111,24 +109,24 @@ const reseat = () => {
     currentNumber = "";
     updatePrevius();
     updateOutput(currentNumber)
-}
+};
 
 const finishOperation = () => {
     if (currentOperator) {
         execute(holdNumber, currentNumber, currentOperator);
-        previusOperation.innerText += ` ${currentNumber} = ${holdNumber}`
+        previousOperation.innerText += ` ${currentNumber} = ${holdNumber}`
     }
     holdNumber = null;
     currentNumber = output.innerText;
     currentOperator = null;
-}
+};
 
 numsBtn.forEach(num => num.addEventListener("click", (e) => addNumber(e.target.innerText)));
 operatorBtns.forEach(btn => 
     btn.addEventListener("click", (e) => selectOperator(e.target.id)));
 result.addEventListener("click", finishOperation)
 clearBtn.addEventListener("click", clear);
-clearAllBtn.addEventListener("click", reseat);
+reseatBtn.addEventListener("click", reseat);
 dotBtn.addEventListener("click", addDot);
 window.addEventListener("keydown", keybordInput)
 updateOutput(0);
